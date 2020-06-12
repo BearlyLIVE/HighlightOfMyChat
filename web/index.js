779110376627617de68331b1439dc57253be1941
BearlyLIVE <66772103+BearlyLIVE@users.noreply.github.com>
2020-06-13 00:11:41 +1000

    Update index.js

--- web/index.js
+++ web/index.js
@@ -170,8 +170,14 @@ ComfyJS.onChat = ( user, message, flags, self, extra ) => {
   }
 };
 
- ComfyJS.onCommand = ( user, command, message, flags, extra ) => {
-        if( flags.broadcaster && command == "test" ) {
+ComfyJS.onCommand = ( user, command, message, flags, extra ) => {
+        if( flags.broadcaster && command == "pat" ) {
+    highlightThisMessage( user, `${ message }`, extra );
+  }
+};
+
+ComfyJS.onCommand = ( user, command, message, flags, extra ) => {
+        if( flags.vip && command == "pat" ) {
     highlightThisMessage( user, `${ message }`, extra );
   }
 };
